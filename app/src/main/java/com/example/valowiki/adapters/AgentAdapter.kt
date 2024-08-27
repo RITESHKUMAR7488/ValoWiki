1a983813215f2ae8c84be9462d562f9dcfc4715a
Nitesh Kumar <niteshman30@gmail.com>
2024-08-27 10:01:57 +0530

    some bug fixes

--- app/src/main/java/com/example/valowiki/adapters/AgentAdapter.kt
+++ app/src/main/java/com/example/valowiki/adapters/AgentAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.valowiki.adapters
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.valowiki.models.Data
 import com.google.api.Context
@@ -8,7 +9,17 @@ class AgentAdapter (
     private val list: List<Data>,
     private val context: Context,
     private val isShort: Boolean?=false
-) :
-        RecyclerView.Adapter<AgentAdapter.ViewHolder>(){
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
 
-        }
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
