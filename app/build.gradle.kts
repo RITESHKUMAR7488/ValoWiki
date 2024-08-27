5412fbccac6f6a91a42238495a5c4c5124398235
RITESHKUMAR7488 <riteshman30@gmail.com>
2024-08-27 09:48:03 +0530

    ddd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation (libs.glide)
+    implementation(libs.retrofit)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
--- app/src/main/java/com/example/valowiki/apiinterface/Api.kt
+++ app/src/main/java/com/example/valowiki/apiinterface/Api.kt
@@ -1,8 +1,10 @@
 package com.example.valowiki.apiinterface
 
 import android.telecom.Call
+import com.example.valowiki.models.ApiAgentResponseModel
+import retrofit2.http.GET
 
-interface Api
+interface Api{
     @GET("agents")
     fun getAgent(): Call<ApiAgentResponseModel>
 }
